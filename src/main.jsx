5e8648b075f46af4413733d972944a0162623bd8
muhammadimam156 <imamaliqadri5@gmail.com>
2026-03-01 02:51:26 +0500

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import 'react-datepicker/dist/react-datepicker.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <App />
         <Toaster
           position="top-right"
